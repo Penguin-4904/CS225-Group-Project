8ca52297d64962e5ea91aff209f1f8c2910da2eb
SpaceFreelancer <98305162+SpaceFreelancer@users.noreply.github.com>
2022-04-08 12:28:34 -0700

    Added comments and made variable names more descriptive

--- Standalone.cpp
+++ Standalone.cpp
@@ -7,16 +7,27 @@ void checkFile()
     time_t now = time(0);
     char *date_time = ctime(&now);
 
-    std::string name = "Stack " + std::string(date_time) + ".txt";
+    // If a file in the current minute has been created
+    std::string present_name = "Stack " + std::string(date_time) + ".txt";
+    // If a file in the minute prior has been created
+    std::string past_name = "Stack " + std::string(date_time - 1) + ".txt";
 
-    ifstream file_to_check(name);
-    if (file_to_check.is_open() != true)
+    // Open the current minute file
+    ifstream file_to_check_present(present_name);
+    // Try to open the past minute file
+    ifstream file_to_check_past(past_name);
+
+    // Check if either file exists
+    if (file_to_check_present.is_open() != true || file_to_check_past.is_open() != true)
     {
       throw string("ER01");
     }
 
-    file_to_check.close();
+    // Close files
+    file_to_check_present.close();
+    file_to_check_past.close();
   }
+  
   catch (string error)
   {
     if (error == "ER01")
